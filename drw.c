ea5b891ac99b1f96ee19991e67919caae5c44aa5
Varun Iyer <varun_iyer@protonmail.com>
2019-12-13 03:07:59 -0800

    color changes

--- drw.c
+++ drw.c
@@ -21,12 +21,12 @@ void clr_txt(void) {
 }
 
 void drw_clr(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
-	u32 bg_color = 0x333333;
+	u32 bg_color = 0x000000;
 	setColor((bg_color >> 16) & 0xFF, (bg_color >> 8) & 0xFF, bg_color & 0xFF);
 	fillRect(x1, y1, x2, y2);
 
 	// Draw squares
-	setColor(127, 127, 127);
+	setColor(0x00, 0x00, 0x00);
 	for(int y = 5; y + 30 < LCD_HEIGHT; y += 40) {
 		for(int x = 5; x + 30 < LCD_WIDTH; x += 40) {
 			if(((y + 30) > y1) && (y < y2) && (x < x2) && ((x + 30) > x1)) {
