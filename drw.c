516730b2c0503f53ec64ee6a74c1a388a3a3e1fd
Rajan <rajansaini691@gmail.com>
2019-12-13 09:31:11 -0800

    hip colors

--- drw.c
+++ drw.c
@@ -10,13 +10,17 @@
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a < _b ? _a : _b; })
+#define BG_COL 0x07, 0x36, 0x42
+#define FG_COL 0x83, 0x94, 0x96
+#define HL_COL 0xd3, 0x36, 0x82
 
 #define NOTEY ((LCD_HEIGHT/2) - 2 * BigFont[1])
 #define NOTEX ((LCD_WIDTH/2))
 void drw_note(char* note, uint16_t hz, int cents) {
 	setFont(BigFont);
-	setColor(0xFF, 0xFF, 0xFF);
-	setColorBg(0x00, 0x00, 0x00);
+	setColor(FG_COL);
+	setColorBg(BG_COL);
+	note[3] = '\0';
 	lcdPrint(note, NOTEX, NOTEY);
 	char hzc[9] = "12345 Hz";
 	char centc[6] = " 12 c";
@@ -31,8 +35,9 @@ void drw_note(char* note, uint16_t hz, int cents) {
 	}
 	centc[1] = ((cents % 100)/10) + '0';
 	centc[2] = (cents % 10) + '0';
-	xil_printf("CENTC %c", centc[0]);
+	hzc[8] = '\0';
 	lcdPrint(hzc, NOTEX - 8 * BigFont[0], NOTEY + 2 * BigFont[1]);
+	centc[5] = '\0';
 	lcdPrint(centc, NOTEX + BigFont[0], NOTEY + 2 * BigFont[1]);
 }
 
@@ -41,19 +46,9 @@ void clr_txt(void) {
 }
 
 void drw_clr(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
-	u32 bg_color = 0x333333;
-	setColor((bg_color >> 16) & 0xFF, (bg_color >> 8) & 0xFF, bg_color & 0xFF);
+	setColor(BG_COL);
 	fillRect(x1, y1, x2, y2);
 
-	// Draw squares
-	setColor(127, 127, 127);
-	for(int y = 5; y + 30 < LCD_HEIGHT; y += 40) {
-		for(int x = 5; x + 30 < LCD_WIDTH; x += 40) {
-			if(((y + 30) > y1) && (y < y2) && (x < x2) && ((x + 30) > x1)) {
-				fillRect(max(x, x1), y, min(x + 30, x2), y + 30);
-			}
-		}
-	}
 }
 
 static uint32_t prev_tun = 420;
@@ -67,16 +62,16 @@ void clr_tun() {
 }
 
 void init_tun(uint32_t tun) {
-	setColor((TUN_COL >> 16) & 0xFF, (TUN_COL >> 8) & 0xFF, TUN_COL & 0xFF);
+	setColor(FG_COL);
 	fillRect(TUN_X1, TUN_Y1, TUN_X2, TUN_Y2);
 	drw_tun(tun);
 }
 
 void drw_tun(uint32_t tun) {
-	setColor((TUN_COL >> 16) & 0xFF, (TUN_COL >> 8) & 0xFF, TUN_COL & 0xFF);
+	setColor(FG_COL);
 	fillRect(tun2pix(prev_tun), TUN_Y1, tun2pix(prev_tun) + TUN_WIDTH, TUN_Y2);
 
-	setColor((TUN_SEL_COL >> 16) & 0xFF, (TUN_SEL_COL >> 8) & 0xFF, TUN_SEL_COL & 0xFF);
+	setColor(HL_COL);
 	fillRect(tun2pix(tun), TUN_Y1, tun2pix(tun) + TUN_WIDTH, TUN_Y2);
 
 	setFont(BigFont);
@@ -87,8 +82,9 @@ void drw_tun(uint32_t tun) {
 	ntext[1] = ((tun % (100)) / 10) + '0';
 	ntext[2] = ((tun % (10))) + '0';
 	ntext[3] = '\0';
-	setColorBg((TUN_COL >> 16) & 0xFF, (TUN_COL >> 8) & 0xFF, TUN_COL & 0xFF);
-	setColor(0x7F, 0x7F, 0x7F);
+	setColorBg(FG_COL);
+	setColor(0x58, 0x6e, 0x75);
+
 	lcdPrint(ntext, 120 - BigFont[0] - BigFont[0]/2, TUN_Y2 - (TUN_HEIGHT -BigFont[1])/2 - BigFont[1]);
 
 	//xil_printf("%d\n\r", tun2pix(tun));
@@ -106,11 +102,11 @@ void drw_dot(uint8_t oct) {
 	uint8_t x = oct2pix(oct);
 
 	// Reset last dot
-	setColor((DOT_COL >> 16) & 0xFF, (DOT_COL >> 8) & 0xFF, DOT_COL & 0xFF);
+	setColor(FG_COL);
 	fillRect(oct2pix(prev_oct), DOT_Y, oct2pix(prev_oct) + DOT_HEIGHT, DOT_Y + DOT_HEIGHT);
 
 	// Mark current dot as selected
-	setColor((DOT_SEL_COL >> 16) & 0xFF, (DOT_SEL_COL >> 8) & 0xFF, DOT_SEL_COL & 0xFF);
+	setColor(HL_COL);
 	fillRect(oct2pix(oct), DOT_Y, oct2pix(oct) + DOT_HEIGHT, DOT_Y + DOT_HEIGHT);
 
 	prev_oct = oct;
@@ -118,7 +114,7 @@ void drw_dot(uint8_t oct) {
 
 // Draws the entire octave-select menu (a row of 10 dots)
 void drw_oct_sel(uint8_t oct) {
-	setColor((DOT_COL >> 16) & 0xFF, (DOT_COL >> 8) & 0xFF, DOT_COL & 0xFF);
+	setColor(FG_COL);
 	for(int i = 0; i < NUM_DOTS; i++) {
 		fillRect(oct2pix(i), DOT_Y, oct2pix(i) + DOT_HEIGHT, DOT_Y + DOT_HEIGHT);
 	}
